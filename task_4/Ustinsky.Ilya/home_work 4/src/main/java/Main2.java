e0f8619fa3f38af28e45c25171123bbb001b41c6
Ustinsky Ilya <onlyokeey@gmail.com>
2016-12-19 19:21:12 +0200

    Ustinsky_Ilya

--- task_4/Ustinsky.Ilya/home_work 4/src/main/java/Main2.java
+++ task_4/Ustinsky.Ilya/home_work 4/src/main/java/Main2.java
@@ -1,11 +1,11 @@
-public class Main2 {
+  public class Main2 {
 
-public static void main(String[] args) {
-int first = Integer.parseInt(args[0]);
-int second = Integer.parseInt(args[1]);
-String function = args[2];
-int result;
-result = Calculator.calculate(first, second, function); 
-System.out.println("The result is: " + result);
+  public static void main(String[] args) {
+     int first = Integer.parseInt(args[0]);
+     int second = Integer.parseInt(args[1]);
+     String function = args[2];
+     int result;
+     result = Calculator.calculate(first, second, function); 
+       System.out.println("The result is: " + result);
 }
 }
--- task_4/Ustinsky.Ilya/home_work 4/src/main/java/Tip.java
+++ task_4/Ustinsky.Ilya/home_work 4/src/main/java/Tip.java
@@ -1,32 +1,31 @@
 public class Tip {
 
 public static void main(String[] args) {
-
-int money = Integer.parseInt(args[0]);
-String function = args[1];
-System.out.println(tip(money, function));
+  int money = Integer.parseInt(args[0]);
+  String function = args[1];
+  System.out.println(tip(money, function));
 }
 public static double tip(int money, String function) {
-double result;
-switch (function) {
- case "terrible":
-  result = (money/100) * 0;
-   break;
- case "poor":
-  result = (money/100) * 5;
-   break;
- case "good":
-  result = (money/100) * 10;
-   break;
-case "great":
-  result = (money/100) * 15;
-   break;
- case "excellent":
-  result = (money/100) * 20;
-   break;
-   default:
-       throw new IllegalArgumentException("excellent");
+   double result;
+    switch (function) {
+      case "terrible":
+        result = (money/100) * 0;
+           break;
+      case "poor":
+        result = (money/100) * 5;
+           break;
+      case "good":
+        result = (money/100) * 10;
+           break;
+      case "great":
+        result = (money/100) * 15;
+           break;
+      case "excellent":
+        result = (money/100) * 20;
+           break;
+      default:
+        throw new IllegalArgumentException("excellent");
 }
-return result;
+      return result;
 }
 }
