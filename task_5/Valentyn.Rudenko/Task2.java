4391fbba7be5568638ccf72acfde51e09ecc80a3
VaRudenko <bajijiu@gmail.com>
2016-12-21 23:55:15 +0200

    Homework212016

---
+++ task_5/Valentyn.Rudenko/Task2.java
@@ -0,0 +1,7 @@
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 1, z = 1; i <= 55; i ++, z = z + 2) {
+            System.out.println(z);
+        }
+    }
+}
---
+++ task_5/Valentyn.Rudenko/Task3.java
@@ -0,0 +1,7 @@
+public class Task3 {
+    public static void main(String[] args) {
+        for (int i = 90; i >= 0; i = i - 5) {
+            System.out.println(i);
+        }
+    }
+}
