2718477e8b8248b4ad6ef35ec7f2c6e638d20646
Oleg Mikhailov <Oleg Mikhailov>
2016-12-21 17:49:01 +0200

    homework

--- task_5/Oleg.Mikhailov/src/main/java/task6/Binary.java
+++ task_5/Oleg.Mikhailov/src/main/java/task6/Binary.java
@@ -1,20 +1,22 @@
 package task6;
 
+import static jdk.nashorn.internal.objects.NativeArray.reverse;
+
 public class Binary {
     public static void main(String[] args) {
         int i = 0;
         int number =30;
-        int[] mas = new int[]{0,0,0,0,0};
+
+        String numb = "";
         while(i < 5) {
 
             int tmp = number / 2;
-            int bin = number - tmp*2;
+            int bin = number % 2;
             number=tmp;
-            mas[i] = bin;
+            numb = numb + bin;
             i++;
         }
-        for(i =4; i>=0;i--) {
-            System.out.print(mas[i]);
-        }
+        String b = new StringBuffer(numb).reverse().toString();
+        System.out.println(b);
     }
 }
