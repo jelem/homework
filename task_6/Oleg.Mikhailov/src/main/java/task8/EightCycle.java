874bbadfd8cbcf810715eb57e4c6db11c2492c84
Oleg Mikhailov <Oleg Mikhailov>
2016-12-25 15:33:22 +0200

    homework6

--- task_6/Oleg.Mikhailov/src/main/java/task8/EightCycle.java
+++ task_6/Oleg.Mikhailov/src/main/java/task8/EightCycle.java
@@ -27,3 +27,32 @@ public class EightCycle {
         }
     }
 }
+package task8;
+
+public class EightCycle {
+    public static void main(String[] args) {
+        int[] array = {12, 123, -32, 5, 1, 5, 56, 23, 9, 644};
+        int max, sum = 0;
+        int sum2 = 0;
+        int min = array[0];
+        max = min;
+        if (array.length != 1) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] > max) {
+                    max = array[i];
+                }
+                if (array[i] < min) {
+                    min = array[i];
+                }
+
+            }
+            for (int i = 0; i < array.length; i++) {
+                sum = sum + array[i];
+            }
+            sum2 = sum - (min + max);
+            System.out.println("Sum=" + sum2);
+        } else {
+            System.out.println(array[0]);
+        }
+    }
+}
