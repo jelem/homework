13ef71f0fb173fc8986dea4da912b34a64528f93
Oleg Mikhailov <Oleg Mikhailov>
2017-01-11 12:47:13 +0200

    homework9

---
+++ task_9/Oleg.Mikhailov/src/main/java/com/hillel/tasks/ArrayTasks.java
@@ -0,0 +1,36 @@
+package com.hillel.tasks;
+
+import java.util.Arrays;
+
+public class ArrayTasks {
+
+
+  public int[] merge(int[] firstArray, int[] secondArray) {
+    int[] result = Arrays.copyOf(firstArray, secondArray.length + firstArray.length);
+    int position = 0;
+
+    for (int i = firstArray.length; i < result.length; i++) {
+      result[i] = secondArray[position];
+      position++;
+    }
+    Arrays.sort(result);
+
+    return result;
+  }
+
+  public boolean sumExists(int[] array, int sum) {
+    boolean result = false;
+
+    int position = 1;
+    for (int i = 0; i < array.length; i++) {
+      for (int j = position; j < array.length; j++) {
+        if (array[i] + array[j] == sum) {
+          result = true;
+          return result;
+        }
+      }
+      position++;
+    }
+    return result;
+  }
+}
