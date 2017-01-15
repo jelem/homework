571519d6e83b41788eeca45624babc1f62932e70
Oleg Mikhailov <Oleg Mikhailov>
2017-01-15 09:12:24 +0200

    homework10

--- task_10/Oleg.Mikhailov/src/main/java/com/hillel/tasks/BinarySearch.java
+++ task_10/Oleg.Mikhailov/src/main/java/com/hillel/tasks/BinarySearch.java
@@ -3,14 +3,22 @@ package com.hillel.tasks;
 public class BinarySearch {
 
   public boolean exists(int[] array, int number) {
+    int low = 0;
+    int height = array.length - 1;
 
     for (int i = 0; i < array.length; i++) {
-      if (array[i] == number) {
+      int middle = low + (height - low) / 2;
+      if (array[middle] == number) {
         return true;
       }
-
+      if (number <= array[middle]) {
+        height = middle - 1;
+      }
+      if (number >= array[middle]) {
+        low = middle + 1;
+      }
     }
-    return false;
 
+    return false;
   }
 }
