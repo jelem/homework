9b423710675f4eeefee742cfd2c650e0b5d1abb3
Timoshenko Oleksandr <aleksandrstim@gmail.com>
2017-01-16 12:05:53 +0200

    MyTask_9

--- task_9/Aleksandr.Timoshenko/src/main/java/com/hillel/tasks/ArrayTasks.java
+++ task_9/Aleksandr.Timoshenko/src/main/java/com/hillel/tasks/ArrayTasks.java
@@ -5,22 +5,47 @@ import java.util.Arrays;
 public class ArrayTasks {
 
   public int[] merge(int[] firstArray, int[] secondArray) {
-    int [] array = Arrays.copyOf(firstArray, firstArray.length + secondArray.length);
-
-    for (int i = firstArray.length, j = 0; i < array.length; i++, j++){
-      array [i] = secondArray [j];
+    int[] array = new int[firstArray.length + secondArray.length];
+    int elementFirstArray = 0;
+    int elementSecondArray = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (firstArray.length <= elementFirstArray) {
+        array[i] = secondArray[elementSecondArray];
+        elementSecondArray++;
+        continue;
+      }
+      if (secondArray.length <= elementSecondArray) {
+        array[i] = firstArray[elementFirstArray];
+        elementFirstArray++;
+        continue;
+      }
+      if (firstArray[elementFirstArray] < secondArray[elementSecondArray]) {
+        array[i] = firstArray[elementFirstArray];
+        elementFirstArray++;
+      } else {
+        array[i] = secondArray[elementSecondArray];
+        elementSecondArray++;
+      }
     }
-    Arrays.sort(array);
     return array;
   }
 
   public boolean sumExists(int[] array, int sum) {
     boolean result = false;
-    for (int i = 0; i < array.length; i++){
-      for (int j = 1; j < array.length; j++){
-        if(array[i]+array[j] == sum){
-          result = true;
-        }
+    int i = 0;
+    int j = array.length - 1;
+    while (i != j) {
+      if (array[i] + array[j] == sum) {
+        result = true;
+        break;
+      }
+      if (array[i] + array[j] > sum) {
+        j--;
+        continue;
+      }
+      if (array[i] + array[j] < sum) {
+        i++;
+        continue;
       }
     }
     return result;
--- task_9/Aleksandr.Timoshenko/src/test/java/com/hillel/tasks/ArrayTasksTest.java
+++ task_9/Aleksandr.Timoshenko/src/test/java/com/hillel/tasks/ArrayTasksTest.java
@@ -7,31 +7,34 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class ArrayTasksTest {
+
   ArrayTasks arrayTasks;
 
   @Before
-  public void setUp(){
+  public void setUp() {
     arrayTasks = new ArrayTasks();
   }
 
   @Test
-  public void testMerge(){
-    int [] a = {1, 3, 5};
-    int [] b = {2, 4, 8, 9, 12};
-    int [] c = {1, 2, 3, 4, 5, 8, 9, 12};
-    assertThat(arrayTasks.merge(a, b), is (c));
+  public void testMerge() {
+    int[] a = {1, 3, 5};
+    int[] b = {2, 4, 8, 9, 12};
+    int[] c = {1, 2, 3, 4, 5, 8, 9, 12};
+    assertThat(arrayTasks.merge(a, b), is(c));
   }
+
   @Test
-  public void testSumExistTrue(){
-    int [] a = {1, 5, 7, 9, 12};
+  public void testSumExistTrue() {
+    int[] a = {1, 5, 7, 9, 12};
     int sum = 12;
-    assertThat(arrayTasks.sumExists(a, sum), is (true));
+    assertThat(arrayTasks.sumExists(a, sum), is(true));
   }
+
   @Test
-  public void testSumExistFalse(){
-    int [] a = {1, 5, 7, 9, 12};
+  public void testSumExistFalse() {
+    int[] a = {1, 5, 7, 9, 12};
     int sum = 3;
-    assertThat(arrayTasks.sumExists(a, sum), is (false));
+    assertThat(arrayTasks.sumExists(a, sum), is(false));
   }
 
 }
