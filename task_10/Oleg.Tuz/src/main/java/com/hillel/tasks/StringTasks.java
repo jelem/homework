e5d10604a72e760958c665c043746b3fa6cee0d0
Oleh Tuz <tuzoleh@gmail.com>
2017-01-24 12:57:18 +0200

    Homework10

--- task_10/Oleg.Tuz/src/main/java/com/hillel/tasks/StringTasks.java
+++ task_10/Oleg.Tuz/src/main/java/com/hillel/tasks/StringTasks.java
@@ -1,5 +1,7 @@
 package com.hillel.tasks;
 
+import java.util.StringJoiner;
+
 public class StringTasks {
   public String replaceEmail(String email) {
 
@@ -12,17 +14,64 @@ public class StringTasks {
 
   public String shortenWords(String sentence) {
 
+      String[] array = sentence.trim().split(" ");
+      StringJoiner stringJoiner = new StringJoiner(" ");
 
-    //sentence = sentence
-    return "";
-  }
+      for (int i = 0; i < array.length; i++) {
+        String word = array[i];
+        String first = String.valueOf(word.charAt(0));
+        String last = String.valueOf(word.charAt(word.length() - 1));
+        int mid = word.length() - 2;
+        if (word.length() <= 4) {
+          stringJoiner.add(word);
+        } else {
+          String one = first + mid + last;
+          stringJoiner.add(one);
+        }
+      }
+      return stringJoiner.toString();
+    }
+
 
   public int countPalindromes(String words) {
-    return 0;
+    String[] array = words.trim().split(",");
+    int count =0;
+
+    for( int i = 0; i< array.length; i++){
+      if (isPolindrome(array[i])) {
+        count++;
+      }
+    }
+
+
+    return count;
+  }
+
+  private boolean isPolindrome(String word) {
+    word = word.trim();
+    for (int i = 0; i < word.length() / 2; i++) {
+      if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+        return false;
+      }
+    }
+    return true;
   }
 
   public int binaryToDecimal(String number) {
-    return 0;
+
+    int result = 0;
+    int power = number.length() - 1;
+    for(int i = 0; i < number.length(); i++){
+      char symbol = number.charAt(i);
+      int digit = Character.getNumericValue(symbol);
+      result = result + (int)Math.pow(2, power) * digit;
+      power--;
+
+    }
+
+    return  result;
+
+
   }
 
   public static void main(String[] args) {
@@ -34,3 +83,4 @@ public class StringTasks {
     System.out.println(result);
   }
 }
+
