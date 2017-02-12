691c0b46cb736a367e99bbb440d9fd4c606acbf5
Timoshenko Oleksandr <aleksandrstim@gmail.com>
2017-02-12 14:39:59 +0200

    HomeWork_Task_13

--- task_13/Aleksandr.Timoshenko/src/test/java/com/hillel/complex/TestComplexNumber.java
+++ task_13/Aleksandr.Timoshenko/src/test/java/com/hillel/complex/TestComplexNumber.java
@@ -20,35 +20,35 @@ public class TestComplexNumber {
   public void testAddition() {
     firstComplexNumber.setComplexNumber(-1, -3);
     ComplexNumber complexResult = firstComplexNumber.addition(new ComplexNumber(3, 2));
-    assertThat(true, is(complexResult.equals(new ComplexNumber(2, -1))));
+    assertThat(complexResult.equals(new ComplexNumber(2, -1)), is (true));
   }
 
   @Test
   public void testSubtraction() {
     firstComplexNumber.setComplexNumber(17, 35);
     ComplexNumber complexResult = firstComplexNumber.subtraction(new ComplexNumber(15, 5));
-    assertThat(true, is(complexResult.equals(new ComplexNumber(2, 40))));
+    assertThat(complexResult.equals(new ComplexNumber(2, 40)), is(true));
   }
 
   @Test
   public void testDivision() {
     firstComplexNumber.setComplexNumber(-1, 2);
     ComplexNumber complexResult = firstComplexNumber.division(new ComplexNumber(2, 1));
-    assertThat(true, is(complexResult.equals(new ComplexNumber(0, -1))));
+    assertThat(complexResult.equals(new ComplexNumber(0, -1)), is (true));
   }
 
   @Test
   public void testMultiplications() {
     firstComplexNumber.setComplexNumber(1, 5);
     ComplexNumber complexResult = firstComplexNumber.multiplications((new ComplexNumber(5, 2)));
-    assertThat(true, is(complexResult.equals(new ComplexNumber(15, 23))));
+    assertThat(complexResult.equals(new ComplexNumber(15, 23)), is (true));
   }
 
   @Test
   public void testSquare() {
     firstComplexNumber.setComplexNumber(5, 5);
     ComplexNumber complexResult = firstComplexNumber.multiplications((firstComplexNumber));
-    assertThat(true, is(complexResult.equals(new ComplexNumber(1, 3))));
+    assertThat(complexResult.equals(new ComplexNumber(1, 3)), is (true));
   }
 
 
