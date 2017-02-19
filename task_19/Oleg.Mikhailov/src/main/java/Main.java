7a23435bffbb425704bd86c150cbc8b65c5f2670
Oleg Mikhailov <Oleg Mikhailov>
2017-02-19 18:09:12 +0200

    homework20

--- task_19/Oleg.Mikhailov/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-
-  public static void main(String[] args) {
-    BrainLuck brainFuck = new BrainLuck("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.");
-    System.out.println(brainFuck.process());
-  }
-}
